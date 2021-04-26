0f1d89ab322f63e4901abc7ad11492d043cd37dc
Nick <Nick@DESKTOP-AR2LNGC>
2021-04-25 23:51:38 -0400

    hw8 asciiart initial completion

---
+++ hw8/asciiart/asciiart/RGBImage.hpp
@@ -0,0 +1,31 @@
+
+#ifndef RGBIMAGE_HPP
+#define RGBIMAGE_HPP
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <sstream>
+using std::istringstream;
+
+class RGBImage {
+public:
+	RGBImage() : _xres(0), _yres(0), _maxval(0) { cout << "DEFAULT" << endl; }
+	RGBImage(string fname);
+	~RGBImage() { cout << "DESTRUCTOR" << endl; }
+	void toASCII();
+private:
+	int _xres, _yres, _maxval;
+	vector<int> _image;
+
+	void dropComment(string& line);
+	void getNextVal(int& val, istringstream& iline, ifstream& fin,
+		string& line, const string& infile);
+};
+
+#endif // !RGBIMAGE_HPP
---
+++ hw8/asciiart/asciiart/asciiart.cpp
@@ -0,0 +1,52 @@
+/**
+ filename:	asciiart.cpp
+ author:	Nick Wirak (adapted from Dr. Genetti work)
+ date:		4/20/2021
+ summary:   This program creates an ascii art representation of a RGB ppm image and
+            outputs the representation to a text file named 'art.txt'. The image file
+            should be titled 'parrot.ppm', and the ppm should be formatted in ascii
+            characters (as opposed to a binary representation). The program should
+            accomodate ppm files formatted in the usual way, and allow for '#'
+            commenting on any line desired.
+
+*/
+
+// EXAMPLE PPM FORMATTING
+// EXAMPLE PPM FORMATTING
+/* This is the first few lines of parrot.ppm
+P3
+# CREATOR: GIMP PNM Filter Version 1.1
+80 80
+255
+0
+0
+1
+10
+11
+8
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+using std::istringstream;
+
+#include "RGBImage.hpp"
+
+int main()
+{
+    cout << "Program to convert a ppm to ascii art." << endl;
+
+    RGBImage ppm ("parrot.ppm");
+    ppm.toASCII();
+
+    cout << "Program finished." << endl;
+}
