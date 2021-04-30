2a3f6833b583760a7e9574f3d64abed5be1eabb3
Nick <Nick@DESKTOP-AR2LNGC>
2021-04-30 03:37:19 -0400

    final question 5 code

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -4,27 +4,88 @@
 #include <iostream>
 using std::cout;
 using std::endl;
-using std::cin;
-#include <sstream>
-using std::istringstream;
-#include <string>
-using std::string;
-#include <vector>
-using std::vector;
+#include <cmath>
+using std::sqrt;
 #include <fstream>
 using std::ifstream;
 using std::ofstream;
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
 
+class MyVector {
+public:
 
+    MyVector() : _xcoord({ 0.0 }), _ycoord({ 0.0 }), _zcoord({ 0.0 }) {}
+    MyVector(string filename) {
+        ifstream fin(filename);
+        if (!fin) {
+            cout << "Error opening file." << endl;
+            fin.close();
+            exit(1);
+        }
+        unsigned int counter(0);
+        while (true) {
+            fin >> _xcoord.at(counter);
+            if (fin) counter++;
+            fin >> _ycoord.at(counter);
+            if (fin) counter++;
+            fin >> _zcoord.at(counter);
+            if (fin) counter++;
+            else {
+                if (fin.eof() && counter % 3 == 0) {
+                    cout << "File Successfully read." << endl;
+                    fin.close();
+                    break;
+                }
+                else {
+                    cout << "Error reading file data." << endl;
+                    fin.close();
+                    exit(2);
+                }
+            }
+        }
+    }
+    ~MyVector() {}
+
+    unsigned int size() const {
+        return _xcoord.size();
+    }
+    double length(unsigned int index) const {
+        return sqrt(_xcoord.at(index) * _xcoord.at(index) + 
+                    _ycoord.at(index) * _ycoord.at(index) +
+                    _zcoord.at(index) * _zcoord.at(index));
+    }
+    void storeLengths(string filename) const {
+        ofstream fout(filename);
+        if (!fout) {
+            cout << "Error creating file." << endl;
+            exit(3);
+        }
+        for (unsigned int i = 0; i < size(); i++) {
+            fout << length(i) << endl;
+            if (!fout) {
+                cout << "Error writing to file." << endl;
+                exit(4);
+            }
+        }
+        fout.close();
+    }
+
+private:
+
+    vector<double> _xcoord;
+    vector<double> _ycoord;
+    vector<double> _zcoord;
+
+};
 
 int main()
 {
 
-    string test("this");
-    string* ptr = &test;
-    cout << ptr << endl;
-    test = "thieh siejfapidj aipejf iaoej fajweio jaioe fjapoj fioajo jaeo fjaopweja";
-    cout << ptr << endl;
+    MyVector vectors("vectors.txt");
+    vectors.storeLengths("lengths.txt");
 
     return 0;
 }
